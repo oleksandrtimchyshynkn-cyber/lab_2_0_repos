efd1542c06607bb79450bc0c853a08778af93e11
oleksandrtimchyshynkn-cyber <oleksandrtimchyshyn.kn@dspu.edu.ua>
2025-10-20 10:12:13 +0300

    first master branch

---
+++ lab_2_0/lab_2_0.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    const double PI = acos(-1.0);
+
+    double alpha_deg, beta_deg;
+    cout << ": ";
+    if (!(cin >> alpha_deg)) return 0;
+    cout << ": ";
+    if (!(cin >> beta_deg)) return 0;
+
+    double alpha = alpha_deg * PI / 180.0;
+    double beta = beta_deg * PI / 180.0;
+
+    double z1 = pow(cos(alpha) - cos(beta), 2) - pow(sin(alpha) - sin(beta), 2);
+
+    double diff_half = (alpha - beta) / 2.0;
+    double z2 = -4.0 * pow(sin(diff_half), 2) * cos(alpha + beta);
+
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    return 0;
+}
